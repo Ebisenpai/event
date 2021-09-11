d05a14d16d6140ed92280b2c04c6a598ef15b71c
Ebisenpai <satsukiabe0328@gmail.com>
2021-09-11 22:09:56 +0900

    [create]create login function #11

---
+++ database/seeds/EventTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class EventTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $event = new \App\User([
+            'title' = title3,
+            'outline' =,
+            'body' =,
+            'user_id'=
+            ])
+    }
+}
