71f3576f390181e610606561e29567ad0b4de2e1
Ebisenpai <satsukiabe0328@gmail.com>
2021-10-10 16:07:22 +0900

    [create]create chats function#22

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -69,18 +69,20 @@
             <div class="col-sm-6">
                 <form action="/chats" method="post">
                     {{ csrf_field() }}
-                    <div class="title">
-                        <h2>Title</h2>
-                        <input type="text" name="events[title]" placeholder="タイトル"/>
+                    <div class="user">
+                        <h2>New message</h2>
+                        <select name="chats[name]" id="user2">
+                            @foreach($unsent_user_id as $unsent_user_id)
+                                <option value="{{$user->get()[$unsent_user_id-1]->id}}">
+                                    {{$user->get()[$unsent_user_id-1]->name}}
+                                </option>    
+                            @endforeach
+                        </select>
                     </div>
-                    <div class="outline">
-                        <h2>outline</h2>
-                        <textarea name="events[outline]" placeholder="概要"/></textarea>
+                    <div class="message">
+                        <textarea name="chats[message]" placeholder="久しぶり。"></textarea>
                     </div>
-                    <div class="body">
-                        <h2>Body</h2>
-                        <textarea name="events[body]" placeholder="今日も1日お疲れさまでした。"></textarea>
-                    </div>
+                    {{--チャットルームの作成機能とhiddenでの情報の送信--}}
                     <input type="submit" value="保存"/>
                 </form>
             </div>
